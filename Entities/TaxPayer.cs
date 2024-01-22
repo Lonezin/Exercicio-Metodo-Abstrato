13e8ea7710a357075483a951883431a93bdaac4e
vinicius morais araujo <vinidev.morais@gmail.com>
2024-01-22 18:39:08 -0300

    created abstract class TaxPayer

---
+++ Entities/TaxPayer.cs
@@ -0,0 +1,18 @@
+namespace Exercicio.Entities
+{
+    public abstract class TaxPayer
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        public TaxPayer()
+        {
+        }
+        public TaxPayer(string name, double anualincome)
+        {
+            Name = name;
+            AnualIncome = anualincome;
+        }
+        public abstract double Tax();
+    }
+}
